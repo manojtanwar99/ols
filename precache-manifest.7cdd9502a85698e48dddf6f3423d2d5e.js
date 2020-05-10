6ae3499532a948d8240eec72e542972d2d586718
manojtanwar99 <manojtanwar99@gmail.com>
2020-05-10 13:11:29 +0530

    Updates

---
+++ precache-manifest.7cdd9502a85698e48dddf6f3423d2d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2c8218cc4bd08af09bb5d515260ff4",
+    "url": "/ols/index.html"
+  },
+  {
+    "revision": "b68381e8bf5ae950898f",
+    "url": "/ols/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f50b330107d6dd6d94d",
+    "url": "/ols/static/js/2.57d3581a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ols/static/js/2.57d3581a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68381e8bf5ae950898f",
+    "url": "/ols/static/js/main.bf1465a6.chunk.js"
+  },
+  {
+    "revision": "3e2f405e2dd809fa88fa",
+    "url": "/ols/static/js/runtime-main.903118d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ols/static/media/logo.5d5d9eef.svg"
+  }
+]);
